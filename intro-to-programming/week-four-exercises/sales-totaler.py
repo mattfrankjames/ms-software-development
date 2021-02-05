d9da57da1253863a79afc36c3a90e918fce9ea40
mattfrankjames <mattfrankjames@gmail.com>
2021-02-05 10:53:41 -0600

    funtioning sales totaller

---
+++ intro-to-programming/week-four-exercises/sales-totaler.py
@@ -0,0 +1,24 @@
+def main():
+    readFile = open(input('Enter sales file name: '), 'r')
+    outFile = input('Enter the total sales file: ')
+    outPutFile = open(outFile, 'w')
+    strings = readFile.readlines()
+    
+    stringLists = []
+    for string in strings:
+        stringLists.append(string[1:].strip().split('$'))
+    
+    for stringList in stringLists:
+        addOne = stringList[0].strip();
+        addTwo = stringList[1].strip();
+        sum = float(addOne) + float(addTwo)
+        sumString = str(sum)
+        print('${0} ${1} ${2}'.format(addOne.rjust(10), addTwo.rjust(10), sumString.rjust(10)), file=outPutFile)
+  
+    
+    outPutFile.close()
+    
+    print('Done writing totals to {0} '.format(outFile))
+    
+    
+main()
