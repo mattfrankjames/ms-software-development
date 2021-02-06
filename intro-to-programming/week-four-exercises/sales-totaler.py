e70574401db3cb78b2276248b4a3650c6a9ff5bd
mattfrankjames <mattfrankjames@gmail.com>
2021-02-06 17:50:27 -0600

    :pencil2: typo in sales-totaler

--- intro-to-programming/week-four-exercises/sales-totaler.py
+++ intro-to-programming/week-four-exercises/sales-totaler.py
@@ -3,22 +3,22 @@ def main():
     outFile = input('Enter the total sales file: ')
     outPutFile = open(outFile, 'w')
     strings = readFile.readlines()
-    
+
     stringLists = []
     for string in strings:
         stringLists.append(string[1:].strip().split('$'))
-    
+
     for stringList in stringLists:
-        addOne = stringList[0].strip();
-        addTwo = stringList[1].strip();
+        addOne = stringList[0].strip()
+        addTwo = stringList[1].strip()
         sum = float(addOne) + float(addTwo)
         sumString = str(sum)
-        print('${0} ${1} ${2}'.format(addOne.rjust(10), addTwo.rjust(10), sumString.rjust(10)), file=outPutFile)
-  
-    
+        print('${0} ${1} ${2}'.format(addOne.rjust(10), addTwo.rjust(
+            10), sumString.rjust(10)), file=outPutFile)
+
     outPutFile.close()
-    
+
     print('Done writing totals to {0} '.format(outFile))
-    
-    
+
+
 main()
