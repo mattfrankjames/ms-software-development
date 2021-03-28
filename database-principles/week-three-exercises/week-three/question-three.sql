07f8daf059ff683388bdd83706d9ecf81e562fe8
mattfrankjames <mattfrankjames@gmail.com>
2021-03-28 15:05:54 -0500

    re-work sql queries

--- database-principles/week-three-exercises/week-three/question-three.sql
+++ database-principles/week-three-exercises/week-three/question-three.sql
@@ -1,18 +1,17 @@
 USE MyWebDB;
 
-INSERT INTO Users VALUES (34987, 'bill@yahoo.com', 'Bill', 'Stevenson'); 
-INSERT INTO Users VALUES (23487, 'steve@yahoo.com', 'Steve', 'Stevenson');
+INSERT INTO dbo.Users (EmailAddress, FirstName, LastName) VALUES ('bill@yahoo.com', 'Bill', 'Stevenson'), ('steve@yahoo.com', 'Steve', 'Stevenson'); 
 
-INSERT INTO Products VALUES(56709, 'Potato');
-INSERT INTO Products VALUES(34529, 'Onion');
 
-INSERT INTO Downloads VALUES(345, 34987, GETDATE(), 'my file', 34529);
-INSERT INTO Downloads VALUES(345, 23487, GETDATE(), 'my file', 56709);
-INSERT INTO Downloads VALUES(345, 23487, GETDATE(), 'my file', 34529);
+INSERT INTO dbo.Products (ProductName) VALUES('Potato'), ('Onion');
+
+INSERT INTO dbo.Downloads (DownloadDate, FileName) VALUES ( GETDATE(), 'my file'),
+( GETDATE(), 'my file'),
+(GETDATE(), 'my file');
 
 SELECT EmailAddress as email_address,
 FirstName as first_name, 
 LastName as last_name, 
 DownloadDate as download_date, 
 ProductName as product_name, 
-ProductName as product_name FROM Users, Products, Downloads ORDER BY EmailAddress, ProductName;
+ProductName as product_name FROM dbo.Users, dbo.Products, dbo.Downloads ORDER BY EmailAddress, ProductName;
